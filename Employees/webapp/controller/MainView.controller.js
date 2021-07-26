d0c683bf9707373f9f8d0792b0e3b10d7acfbf54
naotosx <naotoamari@gmail.com>
2021-07-26 17:14:55 +0000

    Primer commit para rama lista

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,27 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.Employees.controller.MainView", {
-			onInit: function () {
+        var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
 
-			}
-		});
-	});
+            Main.prototype.onValidate = function() {
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length === 6) {
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true);
+                    //inputEmployee.setDescription("Ok");
+                } else {
+                    //inputEmployee.setDescription("Not ok");
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);
+                }
+            };
+        return Main;
+    });
